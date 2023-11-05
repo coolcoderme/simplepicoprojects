67a3efdec2c6249ab36fb27729eefbade4de19a0
coolcoderme <149955694+coolcoderme@users.noreply.github.com>
2023-11-05 09:43:52 -0600

    Add files via upload

---
+++ radomnuber.py
@@ -0,0 +1,9 @@
+import random # imports random in python
+
+from random import randint # imports random from randint
+
+randomnumber = random.randint(1,100000) # selects one number between 1 and 100,000!
+
+# it also stores the random number in a variable called  random number
+
+print (randomnumber) # prints the random number
